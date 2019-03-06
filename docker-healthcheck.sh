0132eff18815713424dfc986c681542bf9fcc735
Todd Cook <tcook@blackducksoftware.com>
2019-03-06 13:12:39 -0500

    HUB-19001:  Changed health check script to echo a message for the non-zero exit codes of pg_isready.

--- docker-healthcheck.sh
+++ docker-healthcheck.sh
@@ -2,20 +2,31 @@
 
 # Using Unix socket connection to test readiness.
 pg_isready --username=blackduck --dbname=bds_hub --port=5432 --quiet
-queryResult=$?
+status=$?
 
-if [ "$queryResult" == "0" ];
+if [ "$status" == "0" ];
 then
   # PostgreSQL is accepting connections.
   exit 0
 fi
 
-if [ "$queryResult" == "1" ];
+if [ "$status" == "1" ];
 then
-  # PostgreSQL is still starting up.
+  echo "PostgreSQL is still starting up."
   exit 0
 fi
 
-# PostgreSQL is not responding.
-exit 1
+if [ "$status" == "2" ];
+then
+	echo "PostgreSQL is not responding."
+	exit 1
+fi
 
+if [ "$status" == "3" ];
+then
+	echo "pg_isready failed."
+	exit 1
+fi
+
+# Should not get here
+exit 1
